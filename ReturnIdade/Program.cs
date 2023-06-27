8284bf879093fc1d444d53ae856d6f8849698461
jhonatacamargol <jhonatapires32@hotmail.com>
2023-06-27 20:33:16 -0300

    primeiro commit

---
+++ ReturnIdade/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace SuaAppIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a sua data de nascimento (no formato dd/mm/aaaa):");
+            string input = Console.ReadLine();
+
+            DateTime dataNascimento;
+            if (DateTime.TryParse(input, out dataNascimento))
+            {
+                int idade = CalcularIdade(dataNascimento);
+                Console.WriteLine($"A sua idade atual é: {idade} anos.");
+            }
+            else
+            {
+                Console.WriteLine("Data de nascimento inválida.");
+            }
+
+            Console.ReadLine();
+        }
+
+        static int CalcularIdade(DateTime dataNascimento)
+        {
+            var hoje = DateTime.Today;
+            int idade = hoje.Year - dataNascimento.Year;
+
+            if (dataNascimento.Date > hoje.AddYears(-idade))
+                idade--;
+
+            return idade;
+        }
+    }
+}
